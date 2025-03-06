176cd169beff20c33eaf6babf8a347d41fa24067
faisal-aslam <faisal.aslam@gmail.com>
2025-03-06 12:34:04 +0500

    Update standardRecursiveDescent.cpp
    Added some comments.

--- recursive-descent/standardRecursiveDescent.cpp
+++ recursive-descent/standardRecursiveDescent.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 
+using namespace std; // Added to avoid using std:: prefix for standard library functions.
+
 // This will hold the input string.
 string input;
 
@@ -9,19 +11,19 @@ int lookahead = 0;
 
 // Function to handle syntax errors.
 void error() {
-	cout << "syntax error" << endl;
-	//TODO this is not correct way of dealing with error. We should recover and continue. 
-	exit(1);
+    cout << "Syntax error at position " << lookahead << endl;
+    // TODO: Implement error recovery instead of exiting.
+    exit(1);
 }
 
 // Function to match the expected character and move the lookahead.
 void match(char expected) {
-	// If the current character matches the expected one, consume it.
-	if (lookahead < input.size() && input[lookahead] == expected) {
-		lookahead++; // Advances to the next character in the input string.
-	} else {
-		error(); // If there's a mismatch, trigger a syntax error.
-	}
+    // If the current character matches the expected one, consume it.
+    if (lookahead < input.size() && input[lookahead] == expected) {
+        lookahead++; // Advances to the next character in the input string.
+    } else {
+        error(); // If there's a mismatch, trigger a syntax error.
+    }
 }
 
 /*
@@ -40,73 +42,76 @@ void C(); // Function for non-terminal C.
 
 // Function for non-terminal S, which has two productions: a A or b.
 void S() {
-	if (input[lookahead] == 'a') { // FIRST(aA)
-		match('a');
-		A();
-	} 
-	else if (input[lookahead] == 'b') { // FIRST (b)
-		match('b');
-	} 
-	// If neither condition is met, it's a syntax error.
-	else {
-		error();
-	}
+    if (input[lookahead] == 'a') { // FIRST(aA)
+        match('a');
+        A();
+    } 
+    else if (input[lookahead] == 'b') { // FIRST(b)
+        match('b');
+    } 
+    // If neither condition is met, it's a syntax error.
+    else {
+        error();
+    }
 }
 
 // Function for non-terminal A, which has three possible productions: B A, C, or ε.
 void A() {
-	if (input[lookahead] == 'b') { //FIRST(BA)
-		B();
-		A(); 
-	} 
-	else if (input[lookahead] == 'c') { //FIRST(C)
-		C();
-	} else if(input[lookahead] == '$') { //FOLLOW(A)
-		//do nothing
-	} else {
-		error();
-	}
+    if (input[lookahead] == 'b') { // FIRST(BA)
+        B();
+        A(); 
+    } 
+    else if (input[lookahead] == 'c') { // FIRST(C)
+        C();
+    } 
+    else if (input[lookahead] == '$') { // FOLLOW(A)
+        // ε production: do nothing.
+    } 
+    else {
+        error();
+    }
 }
 
 // Function for non-terminal B, which matches the terminal 'b'.
 void B() {
-	// If the current symbol is 'b', we consume it.
-	if (input[lookahead] == 'b') { //FIRST(b)
-		match('b');
-	}
-	// If it's not 'b', return false.
-	return false;
+    // If the current symbol is 'b', we consume it.
+    if (input[lookahead] == 'b') { // FIRST(b)
+        match('b');
+    }
+    else {
+        error(); // If it's not 'b', trigger a syntax error.
+    }
 }
 
 // Function for non-terminal C, which matches the terminal 'c'.
-bool C() {
-	// If the current symbol is 'c', we consume it.
-	if (input[lookahead] == 'c') { //FIRST(C)
-		match('c');
-	}
-	// If it's not 'c', return false.
-	return false;
+void C() {
+    // If the current symbol is 'c', we consume it.
+    if (input[lookahead] == 'c') { // FIRST(C)
+        match('c');
+    }
+    else {
+        error(); // If it's not 'c', trigger a syntax error.
+    }
 }
 
 int main() {
-	// Prompt the user to enter the input string.
-	cout << "Enter your input" << endl;
-	cin >> input;
-	
-	// Append an end-of-input marker ($) to indicate the end of the string.
-	input = input + '$';
-	
-	// Start parsing by calling the function for the start symbol S.
-	S();
-	
-	// If the entire input has been parsed and the end marker is reached, the input is valid.
-	if (input[lookahead] == '$') {
-		cout << "Input is part of our language" << endl;
-	} 
-	// If there's more input after the end marker, trigger a syntax error.
-	else {
-		error();
-	}
-	return 0;
+    // Prompt the user to enter the input string.
+    cout << "Enter your input: ";
+    cin >> input;
+    
+    // Append an end-of-input marker ($) to indicate the end of the string.
+    input = input + '$';
+    
+    // Start parsing by calling the function for the start symbol S.
+    S();
+    
+    // If the entire input has been parsed and the end marker is reached, the input is valid.
+    if (input[lookahead] == '$') {
+        cout << "Input is part of our language" << endl;
+    } 
+    // If there's more input after the end marker, trigger a syntax error.
+    else {
+        error();
+    }
+    return 0;
 }
-
